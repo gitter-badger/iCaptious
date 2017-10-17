759eaf2ddff069e02b258c6a2dfd01931984d714
Neo Morina <neomorina@gmail.com>
2017-10-17 16:43:47 +0200

    Routing

---
+++ Route/path_data.php
@@ -0,0 +1,61 @@
+<?php 
+
+namespace iCaptious;
+/**
+* 
+*/
+class Path 
+{
+	public static $REL_URI;
+	public static $call_utf8;
+	public static $path_call;
+	public static $call_parts;
+	public static $GET_DATA;
+
+	function __construct()
+	{
+		if (isset($_SERVER['REQUEST_URI'])) {
+			self::Uri_Path();
+			self::call_parts();
+			self::get_properties();
+		}
+	}
+
+	static function Uri_Path(){
+		self::$REL_URI = rtrim(dirname($_SERVER['SCRIPT_NAME']), '\/');
+	}
+
+	static function call_utf8(){
+		$request_path = explode('?', $_SERVER['REQUEST_URI']);
+		return self::$call_utf8 = substr(urldecode($request_path[0]), strlen(self::$REL_URI) + 1);
+	}
+
+	static function path_call(){
+		return self::$path_call = ((utf8_decode(self::call_utf8()) != basename($_SERVER['PHP_SELF'])) ? 
+			utf8_decode(self::call_utf8()) : '');
+	}
+
+	static function call_parts(){
+		return self::$call_parts = explode('/', self::path_call());
+	}
+
+	static function get_properties(){
+		$request_path = explode('?', $_SERVER['REQUEST_URI']);
+
+		if (!empty($request_path[1])) {
+			$path['query_utf8'] = urldecode($request_path[1]);
+			$path['query'] = utf8_decode(urldecode($request_path[1]));
+			$vars = explode('&', $path['query']);
+			foreach ($vars as $var) {
+			  $t = explode('=', $var);
+			  $path['query_vars'][$t[0]] = $t[1];
+			}
+		} else {
+			$path['query_utf8'] = '';
+			$path['query'] = '';
+			$path['query_vars'] = array();
+		}
+
+		return self::$GET_DATA = $path;
+	}
+}
---
+++ Route/route.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace iCaptious;
+
+Include(dirname(__FILE__).'/path_data.php');
+
+/**
+* 
+*/
+class Route extends \iCaptious\Path
+{
+	
+	function __construct()
+	{
+		# code...
+	}
+
+	/**
+	 * [load description]
+	 * @param  [string]   $route    [It gives the route]
+	 * @param  [function] $callback [Will be called if the route was matched]
+	 * @return [unknown]            [It returns the output of the callback function]
+	 */
+	static function load($route, $callback){
+		$route = self::Sanitize_Route($route);
+		$real_route = self::Sanitize_Route(parent::path_call());
+		$arguments = array();
+
+		foreach ($route as $key => $name) {
+			if (preg_match_all('/{(.*?)}/', $name)) {
+				$route[$key] = $argument = $real_route[$key];
+				$arguments[] = htmlentities($argument); 
+			}
+		}
+
+		if ($route === $real_route) {
+			call_user_func_array($callback, $arguments);
+		}
+		return false;
+	}
+
+	/**
+	 * [Sanitize_Route description]
+	 * @param  [string] $route [It gives the route]
+	 * @return [array]         [It returns the route as array]
+	 */
+	static function Sanitize_Route($route) {
+		# remove slash if it exist at the end of the route
+		$route = trim($route, '/'); 
+		# 	$route = (strpos($route, '/') ? explode('/', $route) : $route);
+		$route = explode('/', $route);
+		return $route;
+	}
+
+	static function isDomain($domain, $callback){
+		if (is_string($domain)) {
+			if ($_SERVER['SERVER_NAME'] == $domain || $_SERVER['SERVER_NAME'] == "www.".$domain) {
+				return call_user_func($callback);
+			}
+		}
+	}
+
+	static function Domain($domain, $callback){
+		return call_user_func_array(__NAMESPACE__ .'\Route::isDomain', func_get_args());
+	}
+
+	static function Secure(){
+		if(empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] == "off"){
+		    $redirect = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+		    header('HTTP/1.1 301 Moved Permanently');
+		    header('Location: ' . $redirect);
+		    exit();
+		}
+	}
+
+	static function inSecure(){
+		if(!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != "off"){
+		    $redirect = 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+		    header('HTTP/1.1 301 Moved Permanently');
+		    header('Location: ' . $redirect);
+		    exit();
+		}
+	}
+
+	static function Redirect($url,$permanently=false){
+		if ($permanently === true) {
+			header('HTTP/1.1 301 Moved Permanently'); // The Redirect will be cached by the browser
+		}
+	    header('Location: ' . $url);
+	    exit();
+	}
+}
