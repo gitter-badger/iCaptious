016388260d03a49c35be25ef114054b3d90d4863
Neo Morina <neomorina@gmail.com>
2017-10-23 14:05:57 +0200

    Update Autoloading

--- autoload.php
+++ autoload.php
@@ -4,30 +4,35 @@ spl_autoload_extensions('.php');
 /**
  * The Autoloader function
  *
- * @param string $class_name The Class Name
+ * @param string $class The Class Name
  *
  * @access public
  */
-spl_autoload_register(function ($class_name) {
-	// Only try to load classes with the WAID namepsace prefix
+spl_autoload_register(function ($class) {	
+	if (substr($class, 0, 10) !== 'iCaptious\\') {
+      /* If the class does not lie under the "ReCaptcha" namespace,
+       * then we can exit immediately.
+       */
+      return;
+    }
 
-	if (strpos($class_name, "iCaptious\\") !== false) {
+    // Replace the backslashes with fontslashes
+    $class = str_replace('\\', '/', $class);
 
-		// split the namespace to an array
-		$namespaces = explode("\\", $class_name);
+	// split the namespace to an array
+	$namespaces = explode("/", $class);
 
-		// unset "iCaptious" since this is as main directory
-		unset($namespaces[0]);
+	// unset "iCaptious" since this is as main directory
+	unset($namespaces[0]);
 
-		// Replace namespace separator with directory separator
-		$directory_file = implode(DIRECTORY_SEPARATOR, $namespaces)."/".end($namespaces);
+	// Replace namespace separator with directory separator
+	$path = implode(DIRECTORY_SEPARATOR, $namespaces)."/".end($namespaces);
 
-		// Get full path of file containing the required class
-		$file = dirname(__FILE__).DIRECTORY_SEPARATOR.$directory_file.".php";	
-		
-		// Load file if it exists
-		if (is_readable($file)){
-			require_once $file;		
-		}
+	// Get full path of file containing the required class
+	$file = dirname(__FILE__).DIRECTORY_SEPARATOR.$path.".php";	
+	
+	// Load file if it exists
+	if (is_readable($file)){
+		require_once $file;		
 	}
 });
